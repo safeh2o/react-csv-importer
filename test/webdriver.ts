f9577c5b5ec26f00f1ab80e7775d16880bbb3482
Mohamed Moselhy <look4d@gmail.com>
2023-10-15 09:03:15 -0400

    default chrome webdriver

--- test/webdriver.ts
+++ test/webdriver.ts
@@ -1,41 +1,17 @@
-import * as path from 'path';
-import * as child_process from 'child_process';
 import { Builder, ThenableWebDriver } from 'selenium-webdriver';
 import * as chrome from 'selenium-webdriver/chrome';
-
-async function getGlobalChromedriverPath() {
-  const yarnGlobalPath = await new Promise<string>((resolve, reject) => {
-    child_process.exec('yarn global dir', { timeout: 8000 }, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result.trim());
-      }
-    });
-  });
-
-  return path.resolve(
-    yarnGlobalPath,
-    './node_modules/chromedriver/lib/chromedriver',
-    process.platform === 'win32' ? './chromedriver.exe' : './chromedriver'
-  );
-}
 
 export function runDriver(): () => ThenableWebDriver {
   let webdriver: ThenableWebDriver | null = null;
 
   // same webdriver instance serves all the tests in the suite
   before(async function () {
-    const chromedriverPath = await getGlobalChromedriverPath();
-
-    const service = new chrome.ServiceBuilder(chromedriverPath);
     const options = process.env.CI
       ? new chrome.Options().headless()
       : new chrome.Options();
 
     webdriver = new Builder()
       .forBrowser('chrome')
-      .setChromeService(service)
       .withCapabilities(options)
       .build();
   });
